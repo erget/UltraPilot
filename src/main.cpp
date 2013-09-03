d048c2363225e891ba832637d3960002f9dd2288
Daniel Lee <lee@isi-solutions.org>
2013-09-03 10:30:45 +0200

    Add usage message

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <string>
 
 #include "wiringPi.h"
 #include "wiringSerial.h"
@@ -16,8 +17,26 @@ int MIN_RANGE = 0;
 int MAX_RANGE = 400;
 int MIN_VIBRATION = 1;
 int MAX_VIBRATION = 1023;
+std::string USAGE =
+"Usage: BatPi [SCALING PARAMETER] \n"
+"\n"
+"Sense the distance to the nearest object using an ultrasonic ranger and \n"
+"report it to the user in the form of vibration. \n"
+"\n"
+"  -h, --help     display this help and exit \n"
+"\n"
+"SCALING PARAMETER controls the output. Possible parameters are: \n"
+"  linear        Vibration is scaled linearly to distance \n"
+"  logarithmic   Vibration is scared logarithmically to distance \n"
+"\n"
+"Report date bugs to lee@isi-solutions.org\n";
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc != 2 || argv[1] == "-h" || argv[1] == "--help") {
+		std::cout << USAGE;
+		return 0;
+	}
+
     wiringPiSetup();
     pinMode(1, PWM_OUTPUT);
 	int serial = serialOpen("/dev/ttyUSB0", 9600);
