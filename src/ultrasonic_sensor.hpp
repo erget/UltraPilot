375228323cc189fbe7aba7cdfd11943f64503f36
Daniel Lee <lee@isi-solutions.org>
2013-12-01 14:06:55 +0100

    Implement UltrasonicGroundSensor

--- src/ultrasonic_sensor.hpp
+++ src/ultrasonic_sensor.hpp
@@ -41,9 +41,41 @@ public:
  */
 class UltrasonicGeneralRanger: public UltrasonicSensor {
 public:
-    UltrasonicGeneralRanger(const double &min,
-            const double &max,
-            const int &serial);
+    UltrasonicGeneralRanger(const double & min,
+            const double & max,
+            const int & serial);
+    void set_alert_level();
+};
+
+/* An ultrasonic rangefinder for detecting ground obstacles.
+ *
+ * The alert level is set according to the variability between distances
+ * measured within the given warn_time. If the deviance between distances
+ * measured exceeds the max_deviance, the alert_level is set to 100. Otherwise
+ * it stays at 0.
+ *
+ * Fields:
+ *  max_deviance: The maximum deviance in measurements allowed within the
+ *                warn_time. As long as the deviance between measurements does
+ *                not exceed this value, the sensor's alert_level is set to 0.
+ *                If the deviance exceeds this level, the alert_level is set to
+ *                100.
+ *  warn_time: The amount of measurements that the sensor continues to warn for
+ *             when its max_deviance is exceeded. For the MaxBotix sensors that
+ *             these classes are written for, this corresponds to tenths of a
+ *             second when the sensors measure continuously.
+ *  observations: The observations from the previous warn_time amount of time.
+ */
+class UltrasonicGroundSensor: public UltrasonicSensor {
+    double max_deviance;
+    int warn_time;
+    std::deque<double> observations;
+public:
+    UltrasonicGroundSensor(const double & min,
+            const double & max,
+            const int & serial,
+            const double & max_deviance,
+            const int & warn_time);
     void set_alert_level();
 };
 
